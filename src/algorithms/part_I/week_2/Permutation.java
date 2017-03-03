038fc5c988311176805a803e0fec96452c602203
Santamarta Esteban, Pablo <pablo.santamarta-esteban@capgemini.com>
2017-03-03 13:29:17 +0100

    week-2 assignment Permutation in progress.

--- src/algorithms/part_I/week_2/Permutation.java
+++ src/algorithms/part_I/week_2/Permutation.java
@@ -5,9 +5,7 @@
  */
 package algorithms.part_I.week_2;
 
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdRandom;
-import java.util.Iterator;
 
 /**
  *
@@ -15,6 +13,31 @@ import java.util.Iterator;
  */
 public class Permutation {
    public static void main(String[] args) {
+//       String[] input = {"AA", "BB", "BB", "BB", "BB", "BB", "CC", "CC"};
+       String[] input = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+       int count = 3;
+       boolean[] pickedInput = new boolean[input.length];
        
+       DequeImpl dequeue = new DequeImpl();
+       
+       for (int i = 0; i < count; i++) {
+           int rand = StdRandom.uniform(0, 2);
+           
+           int index = StdRandom.uniform(0, input.length);
+           
+           while (pickedInput[index] == true) {
+               index = StdRandom.uniform(0, count);
+           }
+           
+           if (rand == 0) {
+               dequeue.addFirst(input[index]);
+           }else {
+               dequeue.addLast(input[index]);
+           }
+           
+           pickedInput[index] = true;
+       }
+       
+       System.out.println(dequeue);
    }
 }
