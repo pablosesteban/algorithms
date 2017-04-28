4ea910ed1a8c3ee49bb9322cc8ebaad868ceb859
Santamarta Esteban, Pablo <pablo.santamarta-esteban@capgemini.com>
2017-04-28 14:52:43 +0200

    week-5 assignment range()

--- src/algorithms/Algorithms.java
+++ src/algorithms/Algorithms.java
@@ -5,7 +5,11 @@
  */
 package algorithms;
 
-import java.util.Arrays;
+import algorithms.part_I.week_5.KdTree;
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
 /**
  *
@@ -29,5 +33,32 @@ public class Algorithms {
         elements[i] = elements[j];
         
         elements[j] = swap;
+    }
+    
+    // add the points that the user clicks in the standard draw window to a kd-tree and draw the resulting kd-tree
+    private static void executeKdTreeVisualizer() {
+        RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
+        StdDraw.enableDoubleBuffering();
+        KdTree kdtree = new KdTree();
+        while (true) {
+            if (StdDraw.mousePressed()) {
+                double x = StdDraw.mouseX();
+                double y = StdDraw.mouseY();
+                StdOut.printf("%8.6f %8.6f\n", x, y);
+                Point2D p = new Point2D(x, y);
+                if (rect.contains(p)) {
+                    StdOut.printf("%8.6f %8.6f\n", x, y);
+                    kdtree.insert(p);
+                    StdDraw.clear();
+                    kdtree.draw();
+                    StdDraw.show();
+                }
+            }
+            StdDraw.pause(50);
+        }
+    }
+    
+    public static void main(String[] args) {
+        executeKdTreeVisualizer();
     }
 }
--- src/algorithms/part_I/week_5/KdTree.java
+++ src/algorithms/part_I/week_5/KdTree.java
@@ -7,8 +7,11 @@ package algorithms.part_I.week_5;
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdDraw;
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -54,6 +57,39 @@ public class KdTree {
             this.p = p;
             this.parent = parent;
         }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            
+            sb.append("Node{" + "point: " + p + ", rectangle: " + rect + ", parent: ");
+            
+            if (parent != null) {
+                sb.append(parent.p);
+            }else {
+                sb.append("null");
+            }
+            
+            sb.append(", lb: ");
+            
+            if (lb != null) {
+                sb.append(lb.p);
+            }else {
+                sb.append("null");
+            }
+            
+            sb.append(", rt: ");
+            
+            if (rt != null) {
+                sb.append(rt.p);
+            }else {
+                sb.append("null");
+            }
+            
+            sb.append("}");
+            
+            return sb.toString();
+        }
     }
     
     // construct an empty set of point
@@ -85,28 +121,33 @@ public class KdTree {
         if (n == null) {
             Node node = new Node(p, parent);
             
-//            if (parent == null) {
-//                // xmin, ymin, xmax, ymax
-//                node.rect = new RectHV(0, 0, 1, 1);
-//            }else {
-//                // check if level is even or odd (instead of using the % operator)
-//                if ((level & 1) == 0) {
-//                    // even level: if the point to be inserted has a smaller x-coordinate than the point at the root, go left; otherwise go right
-//                    int comparison = Double.compare(p.x(), n.p.x());
-//                    
-//                    // left children
-//                    if(comparison <= 0) {
-//                        node.rect = new RectHV(node.parent.rect.xmin(), node.parent.rect.ymin(), node.parent.p.x(), node.parent.rect.ymin());
-//                    }else { // right children
-//                        node.rect = new RectHV(node.parent.p.x(), node.parent.rect.xmax(), node.parent.rect.xmax(), node.parent.rect.ymax());
-//                    }
-//                }else {
-//                    // odd level: if the point to be inserted has a smaller y-coordinate than the point in the node, go left; otherwise go right
-//                    int comparison = Double.compare(p.y(), n.p.y());
-//                    
-//                    
-//                }
-//            }
+            /********************** CREATES THE RECTANGLE WHICH POINT BELONGS TO **********************/
+            RectHV rectangle = null;
+            // root node
+            if (parent == null) {
+                //xmin, ymin, xmas, ymax
+                rectangle = new RectHV(0, 0, 1, 1);
+            }else {
+                // even level (compare to its parent which is splitted by y-coor)
+                if ((level & 1) == 0) {
+                    // bottom children
+                    if (Double.compare(p.y(), parent.p.y()) <= 0) {
+                        rectangle = new RectHV(parent.rect.xmin(), parent.rect.ymin(), parent.rect.xmax(), parent.p.y());
+                    }else { // top children
+                        rectangle = new RectHV(parent.rect.xmin(), parent.p.y(), parent.rect.xmax(), parent.rect.ymax());
+                    }
+                }else { // odd level (compare to its parent which is splitted by x-coor)
+                    // left children
+                    if (Double.compare(p.x(), parent.p.x()) <= 0) {
+                        rectangle = new RectHV(parent.rect.xmin(), parent.rect.ymin(), parent.p.x(), parent.rect.ymax());
+                    }else { // right children
+                        rectangle = new RectHV(parent.p.x(), parent.rect.ymin(), parent.rect.xmax(), parent.rect.ymax());
+                    }
+                }
+            }
+            /********************** CREATES THE RECTANGLE WHICH POINT BELONGS TO **********************/
+            
+            node.rect = rectangle;
             
             return node;
         }
@@ -233,16 +274,18 @@ public class KdTree {
         
         StringBuilder sb = new StringBuilder();
         
-        levelOrder(nodeRoot, sb, 0);
+        levelOrder(nodeRoot, sb, 0, new ArrayList<>());
         
         return sb.toString();
     }
     
-    private void levelOrder(List<Node> nodes, StringBuilder sb, int level) {
+    private List<Node> levelOrder(List<Node> nodes, StringBuilder sb, int level, List<Node> output) {
         if (nodes.isEmpty())
-            return;
+            return null;
         
         List<Node> nextLevelNodes = new LinkedList<>();
+        
+        output.addAll(nodes);
         
         sb.append("level " + level + ": ");
         for (Node node : nodes) {
@@ -269,18 +312,70 @@ public class KdTree {
         
         sb.append("\n");
         
-        levelOrder(nextLevelNodes, sb, ++level);
+        levelOrder(nextLevelNodes, sb, ++level, output);
+        
+        return output;
     }
     
-    // all points that are inside the rectangle
+    /*
+    all points that are inside the rectangle
+    
+    RANGE SEARCH:
+        start at the root and recursively search for points in both subtrees using the following pruning rule:
+            if the query rectangle does not intersect the rectangle corresponding to a node, there is no need to explore that node (or its subtrees)
+            
+            a subtree is searched only if it might contain a point contained in the query rectangle
+    */
     public Iterable<Point2D> range(RectHV rect) {
         if (rect == null)
             throw new NullPointerException("null argument!");
         
-        throw new UnsupportedOperationException("Not implemented yet!");
+        Stack<Point2D> points = new Stack<>();
+        
+        range(root, rect, 0, points);
+        
+        return points;
     }
     
-    // a nearest neighbor in the set to point p; null if the set is empty
+    private void range(Node n, RectHV rect, int level, Stack<Point2D> points) {
+        if (n == null)
+            return;
+        
+        if (!rect.intersects(n.rect))
+            return;
+        
+        if ((level & 1) == 0) {
+            if (Double.compare(rect.xmax(), n.p.x()) <= 0) {
+                range(n.lb, rect, ++level, points);
+            }else {
+                range(n.rt, rect, ++level, points);
+            }
+            
+            if (rect.contains(n.p))
+                points.push(n.p);
+        }else {
+            if (Double.compare(rect.ymax(), n.p.y()) <= 0) {
+                range(n.lb, rect, ++level, points);
+            }else {
+                range(n.rt, rect, ++level, points);
+            }
+            
+            if (rect.contains(n.p))
+                points.push(n.p);
+        }
+    }
+    
+    /*
+    a nearest neighbor in the set to point p; null if the set is empty
+    
+    NEAREST NEIGHBOR SEARCH:
+        start at the root and recursively search in both subtrees using the following pruning rule:
+            if the closest point discovered so far is closer than the distance between the query point and the rectangle corresponding to a node, there is no need to explore that node (or its subtrees)
+    
+            a node is searched only if it might contain a point that is closer than the best one found so far
+            
+            organize your recursive method so that when there are two possible subtrees to go down, you always choose the subtree that is on the same side of the splitting line as the query point as the first subtree to explore—the closest point found while exploring the first subtree may enable pruning of the second subtree
+    */
     public Point2D nearest(Point2D p) {
         if (p == null)
             throw new NullPointerException("null argument!");
@@ -291,23 +386,44 @@ public class KdTree {
     public static void main(String[] args) {
         KdTree kdTree = new KdTree();
         
-        System.out.println("isEmpty: " + kdTree.isEmpty());
-        
         kdTree.insert(new Point2D(0.7, 0.2));
         kdTree.insert(new Point2D(0.5, 0.4));
         kdTree.insert(new Point2D(0.2, 0.3));
         kdTree.insert(new Point2D(0.4, 0.7));
         kdTree.insert(new Point2D(0.9, 0.6));
         
-        System.out.println("isEmpty: " + kdTree.isEmpty());
-        
-        System.out.println("size: " + kdTree.size());
-        
-        System.out.println(kdTree);
-        
         kdTree.draw();
         
-        System.out.println(kdTree.contains(new Point2D(0.4, 0.7)));
-        System.out.println(kdTree.contains(new Point2D(0.4, 0.4)));
+        ArrayList<Node> nodes = new ArrayList<>();
+        nodes.add(kdTree.root);
+        List<Node> output = new ArrayList<>();
+        kdTree.levelOrder(nodes, new StringBuilder(), 0, output);
+        for (Node node : output) {
+            System.out.println(node);
+        }
+        
+        RectHV rectHV = new RectHV(0.1, 0.1, 0.55, 0.45);
+        StdDraw.setPenColor(StdDraw.MAGENTA);
+        rectHV.draw();
+        
+        Iterable<Point2D> range = kdTree.range(rectHV);
+        System.out.println(range);
+        
+//        StdDraw.setPenRadius(0.01);
+//        
+//        StdDraw.setPenColor(StdDraw.GRAY);
+//        output.get(0).rect.draw();
+//        
+//        StdDraw.setPenColor(StdDraw.CYAN);
+//        output.get(1).rect.draw();
+//        
+//        StdDraw.setPenColor(StdDraw.PINK);
+//        output.get(2).rect.draw();
+//        
+//        StdDraw.setPenColor(StdDraw.ORANGE);
+//        output.get(3).rect.draw();
+//        
+//        StdDraw.setPenColor(StdDraw.MAGENTA);
+//        output.get(4).rect.draw();
     }
 }
