4722512a0bf4c224b5df9e8fbb100c1cb841582c
Pablo Santamarta Esteban <pablosesteban@gmail.com>
2018-06-13 17:11:59 +0200

    Digraph order implementation

--- src/main/java/com/pablosesteban/api/impl/DFSOrder.java
+++ src/main/java/com/pablosesteban/api/impl/DFSOrder.java
@@ -6,10 +6,11 @@ package com.pablosesteban.api.impl;
 import java.util.Arrays;
 
 import com.pablosesteban.adt.Graph;
+import com.pablosesteban.adt.Queue;
 import com.pablosesteban.adt.Stack;
 import com.pablosesteban.adt.impl.DirectedGraph;
 import com.pablosesteban.adt.impl.LinkedStack;
-import com.pablosesteban.api.GraphCycle;
+import com.pablosesteban.api.DigraphOrder;
 
 /**
  * A DFS implementation based on the fact that the recursive call stack
@@ -17,24 +18,32 @@ import com.pablosesteban.api.GraphCycle;
  * If it ever finds an edge v-w to a vertex w that is on that stack, it has found
  * a cycle, since the stack is evidence of a path from w to v, and the edge v-w
  * completes the cycle.
+ * The implementation maintains a boolean array onStack[] to keep track of the
+ * vertices for which the recursive call has not completed (by setting onStack[v]
+ * to true on entry and to false on exit), and when it finds an edge v->w to a
+ * vertex w that is on the stack, it has discovered a directed cycle, which it can
+ * recover by following edgeTo[] links.
  */
-public class DFSCycle implements GraphCycle {
+public class DFSOrder implements DigraphOrder {
     private boolean[] marked;
     private int[] edgeTo;
     private Graph g;
     private Stack<Integer> cycle;
     private boolean[] onStack;
     
-    public DFSCycle(Graph g) {
-        marked = new boolean[g.size()];
-        edgeTo = new int[g.size()];
-        this.g = g;
-        onStack = new boolean[g.size()];
+    private Stack<Integer> topologicalOrder;
+    private Queue<Integer> preOrder;
+    private Queue<Integer> postOrder;
+    
+    public DFSOrder(DirectedGraph dg) {
+        marked = new boolean[dg.size()];
+        edgeTo = new int[dg.size()];
+        this.g = dg;
+        onStack = new boolean[dg.size()];
         
-        for (int v = 0; v < g.size(); v++) {
+        for (int v = 0; v < dg.size(); v++) {
             if (!marked[v]) {
-                
-                dfs(g, v);
+                dfs(dg, v);
             }
         }
     }
@@ -48,6 +57,11 @@ public class DFSCycle implements GraphCycle {
     public Iterable<Integer> getCycle() {
         return cycle;
     }
+
+	@Override
+	public Iterable<Integer> order(Order order) {
+		return null;
+	}
     
     @Override
     public String toString() {
@@ -99,27 +113,45 @@ public class DFSCycle implements GraphCycle {
     }
     
     public static void main(String[] args) {
-        Graph g = new DirectedGraph(13);
+    	DirectedGraph dg = new DirectedGraph(13);
+        dg.addEdge(0, 5);
+        dg.addEdge(4, 3);
+        dg.addEdge(0, 1);
+        dg.addEdge(9, 12);
+        dg.addEdge(6, 4);
+        dg.addEdge(5, 4);
+        dg.addEdge(0, 2);
+        dg.addEdge(11, 12);
+        dg.addEdge(9, 10);
+        dg.addEdge(0, 6);
+        dg.addEdge(7, 8);
+        dg.addEdge(9, 11);
+        dg.addEdge(5, 3);
+        dg.addEdge(3, 0);
+        dg.addEdge(12, 9);
         
-        g.addEdge(0, 5);
-        g.addEdge(4, 3);
-        g.addEdge(0, 1);
-        g.addEdge(9, 12);
-        g.addEdge(6, 4);
-        g.addEdge(5, 4);
-        g.addEdge(0, 2);
-        g.addEdge(11, 12);
-        g.addEdge(9, 10);
-        g.addEdge(0, 6);
-        g.addEdge(7, 8);
-        g.addEdge(9, 11);
-        g.addEdge(5, 3);
-        g.addEdge(3, 0);
-        g.addEdge(12, 9);
+        DFSOrder dfso = new DFSOrder(dg);
         
-        DFSCycle dfsc = new DFSCycle(g);
+        System.out.println(dfso);
+        System.out.println("Has cycle: " + dfso.hasCycle());
+        System.out.println();
         
-        System.out.println(dfsc);
-        System.out.println("Has cycle: " + dfsc.hasCycle());
+        dg = new DirectedGraph(13);
+        dg.addEdge(0, 5);
+        dg.addEdge(4, 3);
+        dg.addEdge(0, 1);
+        dg.addEdge(9, 12);
+        dg.addEdge(0, 2);
+        dg.addEdge(11, 12);
+        dg.addEdge(9, 10);
+        dg.addEdge(0, 6);
+        dg.addEdge(7, 8);
+        dg.addEdge(9, 11);
+        dg.addEdge(3, 0);
+        
+        dfso = new DFSOrder(dg);
+        
+        System.out.println(dfso);
+        System.out.println("Has cycle: " + dfso.hasCycle());
     }
 }
