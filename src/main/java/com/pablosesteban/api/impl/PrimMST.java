17550700bca9b8efed4861320d9ead122fbf0ef1
Pablo Santamarta Esteban <pablosesteban@gmail.com>
2018-07-11 17:24:43 +0200

    MST implementation fixes.

--- src/main/java/com/pablosesteban/api/impl/PrimMST.java
+++ src/main/java/com/pablosesteban/api/impl/PrimMST.java
@@ -10,7 +10,7 @@ import com.pablosesteban.adt.impl.LinkedQueue;
 import com.pablosesteban.adt.impl.UndirectedWeightedGraph;
 import com.pablosesteban.api.WeightedGraphMinimumSpanningTree;
 
-import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.IndexMinPQ;
 
 /**
  * A MST implementation based on the Prim's Algorithm and assuming that the weighted graph is connected.
@@ -18,22 +18,26 @@ import edu.princeton.cs.algs4.MinPQ;
  * tree, then add number of vertices - 1 edges to it, always taking next vertex the minimum weighted edge
  * that connects a vertex on the tree to a vertex not yet on the tree (crossing edge).
  * An ineligible edge connects two tree vertices.
- * The algorithm is backed by three data structures:<ul>
+ * The algorithm is backed by these data structures:<ul>
  * <li>A vertex-indexed boolean array, where each entry is true if the vertex is currently on the tree.</li>
- * <li>A queue to collect MST edges.</li>
- * <li>A priority queue, that compares edges by weight, to store crossing edges (perhaps also some
- * ineligible ones.)</li></ul>
- * The algorithm take an edge from the priority queue and, if it is not ineligible, add it to the tree and
- * updates the set of crossing edges with adjacent vertices of the vertex that this edge leads to.
- * Any edge connecting the vertex just added to a tree vertex that is already on the priority queue now becomes
- * ineligible.
+ * <li>An indexed priority queue, that compares edges by weight, to store only crossing edges.</li></ul>
+ * <li>A vertex-indexed array of edges (edgeTo) and a vertex-indexed double array (weightTo) where if
+ * vertex v is not on the tree but has at least one edge connecting it to the tree, then edgeTo[v] is the
+ * shortest edge connecting v to the tree, and weightTo[v] is the weight of that edge. All such vertices
+ * v are maintained on the indexed priority queue, as an index v associated with the weight of edgeTo[v].</li>
+ * The algorithm maintains on the indexed priority queue just one edge for each non-tree vertex w, i.e.
+ * the shortest edge that connects it to the tree. Any longer edge connecting w to the tree will become
+ * ineligible at some point, so there is no need to keep it on the indexed priority queue.
+ * 
  * The implementation uses space proportional to twice the number of edges (E) and time proportional to E logE,
  * in the worst case, to compute the MST.
  */
 public class PrimMST implements WeightedGraphMinimumSpanningTree {
 	private boolean[] marked;
 	private Queue<Edge> edges;
-	private MinPQ<Edge> crossingEdges;
+	private IndexMinPQ<Double> crossingEdges;
+	private Edge[] edgeTo;
+	private double[] weightTo;
 	private double weight;
 
 	/**
@@ -43,25 +47,29 @@ public class PrimMST implements WeightedGraphMinimumSpanningTree {
 	 */
 	public PrimMST(WeightedGraph wg) {
 		marked = new boolean[wg.size()];
+		crossingEdges = new IndexMinPQ<>(wg.size());
+		edgeTo = new Edge[wg.size()];
+		weightTo = new double[wg.size()];
 		edges = new LinkedQueue<>();
-		crossingEdges = new MinPQ<>();
 
-		addCrossingEdges(wg, 0);
+		for (int i = 0; i < weightTo.length; i++) {
+			weightTo[i] = Double.POSITIVE_INFINITY;
+		}
+
+		weightTo[0] = 0.0;
+		crossingEdges.insert(0, 0.0);
 
 		while (!crossingEdges.isEmpty() ) {
-			Edge minWeightedEdge = crossingEdges.delMin();
-			
-			int to = minWeightedEdge.getTo();
-			
-			if (marked[to]) {
-				continue;
+			addCrossingEdges(wg, crossingEdges.delMin());
+		}
+
+		for (int i = 0; i < edgeTo.length; i++) {
+			Edge e = edgeTo[i];
+
+			if (e != null) {
+				weight += e.getWeight();
+				edges.enqueue(e);
 			}
-			
-			edges.enqueue(minWeightedEdge);
-			
-			weight += minWeightedEdge.getWeight();
-			
-			addCrossingEdges(wg, to);
 		}
 	}
 
@@ -77,31 +85,48 @@ public class PrimMST implements WeightedGraphMinimumSpanningTree {
 
 	@Override
 	public String toString() {
-		return getClass().getSimpleName() + " {\nmarked: " + Arrays.toString(marked) + ",\nedges: " + edges + ",\nweight: " + weight + ",\ncrossingEdges: " + crossingEdges + "\n}";
+		return getClass().getSimpleName() + " {\nmarked: " + Arrays.toString(marked) + ",\nedges: " + getEdges() + ",\nweight: " + weight + ",\ncrossingEdges: " + crossingEdges + "\n}";
 	}
 
 	/**
-	 * Marking the vertex as visited and then putting all of its incident edges that are
-	 * not ineligible onto the priority queue, thus ensuring that the priority queue contains
-	 * the crossing edges from tree vertices to non-tree vertices (perhaps also some ineligible edges)
+	 * For a vertex v from the indexed priority queue, checks each edge v-w on its adjacency list.
+	 * If w is marked, the edge is ineligible.
+	 * If it is not on the priority queue or its weight is lower than the current best-known edgeTo[w],
+	 * the code updates the data structures to establish v-w as the best-known way to connect v to the
+	 * tree.
 	 * 
 	 * @param wg a weighted graph
 	 * @param v a vertex
 	 */
 	private void addCrossingEdges(WeightedGraph wg, int v) {
 		marked[v] = true;
-		
+
 		for (Edge e : wg.getIncidentEdges(v)) {
-			if (!marked[e.getTo()]) {
-				crossingEdges.insert(e);
+			int toVertex = e.getTo();
+
+			if (marked[toVertex]) {
+				continue;
+			}
+
+			if (e.getWeight() < weightTo[toVertex]) {
+				edgeTo[toVertex] = e;
+				weightTo[toVertex] = e.getWeight();
+
+				if (crossingEdges.contains(toVertex)) {
+					crossingEdges.changeKey(toVertex, weightTo[toVertex]);
+				}else {
+					crossingEdges.insert(toVertex, weightTo[toVertex]);
+				}
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		UndirectedWeightedGraph uwg = new UndirectedWeightedGraph("weighted_graph_tiny.txt");
-		
+
 		PrimMST mst = new PrimMST(uwg);
 		System.out.println(mst);
+
+		IndexMinPQ<String> impq = new IndexMinPQ<>(10);
 	}
 }
