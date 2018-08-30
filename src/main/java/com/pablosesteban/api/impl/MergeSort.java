f5bfe090ddb11cf2f726af6e7ed88b5e3afaf962
Pablo Santamarta Esteban <pablosesteban@gmail.com>
2018-08-30 10:04:33 +0200

    MergeSort improvements: use insertion sort for small sub-arrays and check if array is already ordered before merge.

--- src/main/java/com/pablosesteban/api/impl/MergeSort.java
+++ src/main/java/com/pablosesteban/api/impl/MergeSort.java
@@ -11,17 +11,23 @@ import com.pablosesteban.api.Sort;
  * A recursive sort implementation based on an abstract in-place merge.
  * It is one of the best-known examples of the utility of the DIVIDE-AND-CONQUER paradigm
  * for efficient algorithm design.
- * It is a simple recursive sort method to sort an array which consist on dividing it into
+ * It is a simple recursive method to sort an array which consist on dividing it into
  * two halves, sort the two halves (recursively), and then merge the results.
  * This recursive code is the basis for an inductive proof that the algorithm sorts the
  * array: if it sorts the two sub-arrays, it sorts the whole array, by merging together the
  * sub-arrays.
  * It guarantees to sort any array of N items in time proportional to N log N, but uses extra
- * space proportional to N.
+ * space proportional to N. A way to understand it is to examine the tree where each node
+ * depicts a subarray for which sort does a merge. The tree has precisely n levels. For k from
+ * 0 to n-1, the k-th level from the top depicts 2^k subarrays, each of length 2^n-k, each of
+ * which thus requires at most 2^n-k compares for the merge. Thus we have 2^k*2^n-k = 2^n total
+ * cost for each of the n levels, for a total of n*2^n = N lgN.
  *
  * @param <T> type of elements in the array
  */
 public class MergeSort<T extends Comparable<T>> implements Sort<T> {
+	private static int CUTOFF_LENGTH_INSERTION_SORT = 7;
+	
 	// auxiliary array for merges
 	private T[] elements;
 
@@ -44,7 +50,10 @@ public class MergeSort<T extends Comparable<T>> implements Sort<T> {
 	 * @param hi highest index in the array
 	 */
 	private void mergeSort(T[] arr, int lo, int hi) {
-		if (hi <= lo) {
+		// use insertion sort for small sub-arrays (faster than merge sort)
+		if (hi <= lo + CUTOFF_LENGTH_INSERTION_SORT - 1) {
+			SortUtils.insertionSort(arr);
+			
 			return;
 		}
 
@@ -56,8 +65,11 @@ public class MergeSort<T extends Comparable<T>> implements Sort<T> {
 		// sort right half
 		mergeSort(arr, mid + 1, hi);
 
-		// merge both halves
-		merge(arr, lo, mid, hi);
+		// check whether the array is already in order
+		if (arr[mid].compareTo(arr[mid+1]) > 0) {
+			// merge both halves
+			merge(arr, lo, mid, hi);
+		}
 	}
 
 	/**
